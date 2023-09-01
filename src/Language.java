625e8c4ce8379a211c450713b189548afbab2b47
Endi Triço <enditrico@gmail.com>
2023-09-01 16:52:15 +0200

    Updated

--- src/Language.java
+++ src/Language.java
@@ -1,2 +1,136 @@
-package PACKAGE_NAME;public class Language {
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
+
+public class Language {
+    private String name;
+    private int NGramModel;
+    private String path;
+    private final Map<String, Integer> histogram;
+    private String text;
+    private double vector;
+    private final Object vectorLock = new Object();
+
+    public Language(String name, int NGramModel, String path) {
+        setName(name);
+        setNGramModel(NGramModel);
+        setPath(path);
+        setText("");
+        histogram = new ConcurrentHashMap<>();
+        vector = 0.0D;
+        getTextFiles();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Integer> getHistogram() {
+        return histogram;
+    }
+
+    public int getNGramModel() {
+        return NGramModel;
+    }
+
+    public void setNGramModel(int NGramModel) {
+        this.NGramModel = NGramModel;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setVector(double vector) {
+        synchronized (vectorLock) {
+            this.vector = vector;
+        }
+    }
+
+    public double getVector() {
+        synchronized (vectorLock) {
+            return vector;
+        }
+    }
+
+    private void getTextFiles() {
+        File folder = new File(getPath());
+
+        FilenameFilter textFileFilter = (dir, name) -> name.toLowerCase().endsWith(".txt");
+
+        File[] textFiles = folder.listFiles(textFileFilter);
+
+        StringBuilder contentOfTextFiles = new StringBuilder();
+
+        if (textFiles != null && textFiles.length > 0) {
+            Arrays.stream(textFiles).forEach(textFile -> {
+                try {
+                    String fileContent = Files.readString(textFile.toPath());
+                    contentOfTextFiles.append(fileContent).append(" ");
+                } catch (IOException e) {
+                    System.err.println("Error reading file: " + textFile.getName() + " on folder " + getName());
+                }
+            });
+        }
+        removeDigitsPunctuation(contentOfTextFiles);
+    }
+
+    private void removeDigitsPunctuation(StringBuilder originalText){
+        setText(originalText.toString()
+                .toLowerCase()
+                .replaceAll("\\p{Punct}", " ")
+                .replaceAll("\\d", " ")
+                .replaceAll("\\s{2,}", " ")
+                .trim());
+        tokenize();
+    }
+
+    private void tokenize(){
+        String[] words = getText().split(" ");
+
+        Arrays.stream(words).forEach(word -> {
+            if (word.length() >= getNGramModel()) {
+                IntStream.range(0, word.length() - getNGramModel() + 1)
+                        .mapToObj(index -> word.substring(index, index + getNGramModel()))
+                        .forEach(this::populateHistogram);}
+        });
+
+        calculateVector();
+    }
+
+    public void populateHistogram(String token){
+        synchronized (histogram) {
+            histogram.put(token, histogram.getOrDefault(token, 0) + 1);
+        }
+    }
+
+    public void calculateVector() {
+        int sum;
+
+        synchronized (histogram) {
+            sum = histogram.values().stream()
+                    .mapToInt(value -> value * value)
+                    .sum();
+        }
+
+        setVector(Math.sqrt(sum));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,126 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.concurrent.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static final DecimalFormat decimalFormat = new DecimalFormat("0.00000");
+    static Map<String, List<Double>> languageWithSimilarityAndAngle = new ConcurrentHashMap<>();
+    static Map<String, String> languagePrefixNames = new ConcurrentHashMap<>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void main(String[] args) throws IOException {
+        Path rootDir = Paths.get("src\\LocalFolder");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the N-Gram Model (between 1 and 3 inclusive): ");
+        int NGramModel = validateInput(input.nextInt());
+
+        input.close();
+
+        updateLanguagePrefixNames();
+        List<Language> folders = new ArrayList<>();
+        final Language[] mysteryLanguage = new Language[1];
+
+        ExecutorService executorService1 = Executors.newCachedThreadPool();
+
+        Files.walkFileTree(rootDir, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                if(dir.getFileName().toString().equals("LocalFolder"))
+                     mysteryLanguage[0] = new Language("mystery", NGramModel, dir.toString());
+                else
+                    folders.add(new Language(languagePrefixNames.getOrDefault(dir.getFileName().toString(), dir.getFileName().toString()), NGramModel, dir.toString()));
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        executorService1.shutdown();
+        try {
+            if (!executorService1.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS))
+                System.out.println("ExecutorService1 did not terminate in time.");
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
         }
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        folders.forEach(folder -> executorService.submit(() -> calculateDocumentDistance(mysteryLanguage[0], folder)));
+
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS))
+                System.out.println("ExecutorService did not terminate in time.");
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        languageWithSimilarityAndAngle.forEach((key, value) ->
+            System.out.printf("Language:%-16sSimilarity:%-15.5fAngle:%-10.5f\n", key, value.get(0), value.get(1)));
+
+        highestSimilarity();
+    }
+
+    private static void updateLanguagePrefixNames(){
+        languagePrefixNames.put("al", "Albanian");
+        languagePrefixNames.put("de", "German");
+        languagePrefixNames.put("en", "English");
+        languagePrefixNames.put("fr", "French");
+        languagePrefixNames.put("gr", "Greek");
+        languagePrefixNames.put("it", "Italian");
+    }
+
+    public static void calculateDocumentDistance(Language mystery, Language language){
+        int dotProduct = mystery.getHistogram().entrySet().stream()
+                .filter(entry -> language.getHistogram().containsKey(entry.getKey()))
+                .mapToInt(entry -> entry.getValue() * language.getHistogram().get(entry.getKey()))
+                .sum();
+
+        double similarity = dotProduct / (mystery.getVector() * language.getVector());
+        double angle = Math.toDegrees(Math.acos(similarity));
+
+        languageWithSimilarityAndAngle.put(language.getName(), new ArrayList<>(List.of(
+                Double.parseDouble(decimalFormat.format(similarity)),
+                Double.parseDouble(decimalFormat.format(angle)))));
+    }
+
+    public static void highestSimilarity(){
+        double maxSimilarity = languageWithSimilarityAndAngle.values().stream()
+                .mapToDouble(doubles -> doubles.get(0))
+                .max()
+                .orElse(Double.NaN);
+
+        double minAngle = languageWithSimilarityAndAngle.values().stream()
+                .mapToDouble(doubles -> doubles.get(1))
+                .min()
+                .orElse(Double.NaN);
+
+        List<String> languageName = languageWithSimilarityAndAngle.entrySet().stream()
+                .filter(entry -> entry.getValue().get(0) == maxSimilarity)
+                .map(Map.Entry::getKey)
+                .toList();
+
+        printResult(languageName, maxSimilarity, minAngle);
+    }
+
+    public static void printResult(List<String> languageName, double maxSimilarity, double minAngle){
+        if(languageName.isEmpty()){
+            System.out.println("\nThere is no language similar to mystery.txt");
+        } else if (languageName.size() == 1) {
+            System.out.println("\nThere is only one language with the same highest similarity value.");
+            languageName.forEach(language ->
+                    System.out.printf("Nearest Language with mystery.txt is: %s Language with Similarity: %.5f and Angle: %.5f.", language, maxSimilarity, minAngle));
+        } else{
+            System.out.println("\nThere are " + languageName.size() + " languages with the same highest similarity value.");
+            languageName.forEach(language ->
+                    System.out.printf("Nearest Language with mystery.txt is: %s Language with Similarity: %.5f and Angle: %.5f.\n", language, maxSimilarity, minAngle));
+        }
+    }
+
+    public static int validateInput(int NGramModel){
+        if (NGramModel >= 1 && NGramModel <= 3)
+            return NGramModel;
+        return 2;
     }
 }
